e883dca7e3e5880d7e0abd97cb38e3682cf2e457
Fengda HUANG <h@phodal.com>
2016-05-31 02:42:56 +0800

    add start button

--- www/js/MainMenu.js
+++ www/js/MainMenu.js
@@ -7,25 +7,23 @@ Growth.MainMenu.prototype = {
   create: function () {
     this.game.stage.backgroundColor = "#5F6369";
 
-    button = this.game.add.button(this.game.world.centerX, this.game.world.centerY, 'button', this.startGame, this, 1, 0, 2);
+    button = this.game.add.button(this.game.world.centerX, this.game.world.centerY, 'start-button', this.startGame, this, 1, 0, 2);
     button.anchor.setTo(0.5, 0.5);
 
     deceleon = this.game.add.button(this.game.world.width, this.game.world.centerY, 'deceleon', this.startGame, this, 1, 0, 3);
     deceleon.anchor.setTo(1, 0.5);
 
     var style = {
-      font: "32px '微软雅黑'",
-      fill: "#1d1500",
+      font: "20px '微软雅黑'",
+      fill: "#fff",
       wordWrap: true,
       wordWrapWidth: this.game.width,
       align: "center"
     };
 
-    this.welcomeText = this.game.add.text(this.game.height, this.game.width / 2, "欢迎来到Dream Coastline", style);
+    this.welcomeText = this.game.add.text(this.game.width / 2, this.game.height - 40, "欢迎来到 Dream Coastline \n 一个游戏中的游戏", style);
     this.welcomeText.anchor.set(0.5);
     this.welcomeText.alpha = 1;
-
-    this.game.add.tween(this.welcomeText).to({alpha: 0}, 4000, Phaser.Easing.Linear.None, true)
   },
 
   update: function () {
--- www/js/Preloader.js
+++ www/js/Preloader.js
@@ -5,12 +5,12 @@ Growth.Preloader = function (game) {
 
 Growth.Preloader.prototype = {
   preload: function () {
-    this.preloadBar = this.add.sprite(0, 100, 'preloaderBar');
+    this.preloadBar = this.add.sprite(0, this.game.height - 100, 'preloaderBar');
     this.load.setPreloadSprite(this.preloadBar);
 
     this.load.image('logo', 'images/logo.png');
     this.load.image('background', 'images/trok.jpg');
-    this.load.spritesheet('button', 'images/button_sprite_sheet.png', 193, 71);
+    this.load.image('start-button', 'images/start-button.png');
     this.load.spritesheet('deceleon', 'images/deceleon.png', 256, 256);
   },
   create: function () {
@@ -22,9 +22,11 @@ Growth.Preloader.prototype = {
     logo.anchor.setTo(0.5, 0.5);
     logo.alpha = 0;
 
+    this.preloadBar.alpha = 0;
+
     var tween = self.game.add.tween(logo).to({alpha: 1}, 2000, "Linear", true);
     tween.onComplete.add(function () {
-      self.game.time.events.add(1000, function () {
+      self.game.time.events.add(800, function () {
         self.state.start('MainMenu');
       }, self);
     }, self);
