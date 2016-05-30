4ea8af402002c89236949347a4f988135e1a7d6c
Fengda HUANG <h@phodal.com>
2016-05-31 00:38:48 +0800

    add utf8 font for test

--- www/js/Game.js
+++ www/js/Game.js
@@ -9,6 +9,19 @@ Growth.Game.prototype = {
     var background = this.game.add.image(0, 0, 'background');
     background.height = this.game.height;
     background.width = this.game.width;
+
+    var style = {
+      font: "32px '微软雅黑'",
+      fill: "#ddd",
+      wordWrap: true,
+      wordWrapWidth: this.game.width,
+      align: "center",
+      alpha: 0.5,
+      opacity: 0.5
+    };
+
+    var text = this.game.add.text(this.game.height, this.game.width / 2, "欢迎来到Dream Coastline", style);
+    text.anchor.set(0.5);
   },
 
   gameOver: function () {
